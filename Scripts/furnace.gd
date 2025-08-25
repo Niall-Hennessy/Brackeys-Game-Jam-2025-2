549ca4db3bb691e2277f80e6072009b4f691336b
Niall Hennessy <48255340+Niall-Hennessy@users.noreply.github.com>
2025-08-25 11:07:53 +0100

    Added the ability to chose how many Biscuits you consume
    Added the ability to choose how many biscuits you consume.
    
    Refactored ui elements to be Child of GUI Control Node that takes care of the logic of the GUI
    
    GUI now directly accesses GameManager Functions so should be careful about that and look into refactoring this in order to pass values more safely
    
    Fixed a bug where player could no longer collide with Furnace

--- Scripts/furnace.gd
+++ Scripts/furnace.gd
@@ -1,11 +1,10 @@
 extends Node2D
 
 func _on_proximity_trigger_body_entered(body: Node2D) -> void:
-	print("Test")
 	$BiscuitButton.visible = true
 
 func _on_proximity_trigger_body_exited(body: Node2D) -> void:
 	$BiscuitButton.visible = false
 
 func _on_button_pressed() -> void:
-	GameManager.biscuit_gained.emit()
+	GameManager.gain_biscuit(3)
--- game_manager.gd
+++ game_manager.gd
@@ -2,17 +2,22 @@ extends Node
 
 @export var biscuits: int = 0
 @export var turn: int = 1
+@export var player_distance: int = 1000
+@export var winter_distance: int = 0
 
-signal biscuit_gained
-signal turn_ended
+var rng = RandomNumberGenerator.new()
 
-func _ready() -> void:
-	GameManager.biscuit_gained.connect(gain_biscuit)
-	GameManager.turn_ended.connect(next_turn)
+var my_array = [100, 200, 300, 400, 500]
+var weights = PackedFloat32Array([0.5, 1, 2, 1.5, 1]) #https://docs.godotengine.org/en/4.4/classes/class_randomnumbergenerator.html
 	
-func gain_biscuit() -> void:
-	biscuits += 1
+func gain_biscuit(num_biscuits: int) -> void:
+	biscuits += num_biscuits
 
-func next_turn() -> void:
-	biscuits -= 3
+func next_turn(consume_num_biscuits: int) -> void:
+	if biscuits < consume_num_biscuits:
+		return
+	
+	biscuits -= consume_num_biscuits
+	player_distance += consume_num_biscuits * 100
+	winter_distance += my_array[rng.rand_weighted(weights)]
 	turn += 1
