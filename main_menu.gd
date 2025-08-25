34bcf2a8b68003ad3622274f942305114cfa5695
Joe Clarke <joeclarke9951@gmail.com>
2025-08-25 21:39:54 +0100

    add basic main menu screen

---
+++ main_menu.gd
@@ -0,0 +1,12 @@
+extends Node2D
+
+
+func _on_start_game_button_pressed() -> void:
+	get_tree().change_scene_to_file("res://Scenes/Camps/test_level.tscn")
+
+
+
+func _on_quit_button_pressed() -> void:
+	get_tree().quit()
+
+# add either a settings menu or mute SFX + mute music buttons?
