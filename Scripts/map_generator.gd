253e272829e4a2977b8160f08eed591b17c68756
Niall Hennessy <48255340+Niall-Hennessy@users.noreply.github.com>
2025-08-28 17:57:25 +0100

    Introduced a Zone based weighted Randomness to the World Generator

--- Scripts/map_generator.gd
+++ Scripts/map_generator.gd
@@ -3,27 +3,31 @@ extends Node
 #https://www.youtube.com/watch?v=7HYu7QXBuCY
 
 @export var plains_camp_weight = 11
-@export var arid_camp_weight = 7
-@export var mountain_camp_weight = 8
-@export var river_camp_weight = 8
+@export var arid_camp_weight = 3
+@export var mountain_camp_weight = 2
+@export var river_camp_weight = 5
 @export var forest_camp_weight = 8
 
 const X_Dist := 30
 const Y_Dist := 25
 const PLACEMENT_RANDOMNESS := 5
 const FLOORS := 15
-const MAP_HEIGHT := 4
-const PATHS := 3
+const MAP_HEIGHT := 10
+const PATHS := 8
 
 var map_data: Array[Array]
 
 var rng = RandomNumberGenerator.new()
 var camp_biomes_array = [Camp.Biome.PLAINS, Camp.Biome.ARID, Camp.Biome.MOUNTAIN, Camp.Biome.RIVER, Camp.Biome.FOREST]
-var weights = PackedFloat32Array([plains_camp_weight, arid_camp_weight, mountain_camp_weight, river_camp_weight, forest_camp_weight])
+var early_weights = PackedFloat32Array([plains_camp_weight, arid_camp_weight, mountain_camp_weight, river_camp_weight, forest_camp_weight])
+var early_mid_weights = PackedFloat32Array([plains_camp_weight * 0.6, arid_camp_weight * 0, mountain_camp_weight, river_camp_weight * 0.6, forest_camp_weight * 2.5])
+var mid_weights = PackedFloat32Array([plains_camp_weight * 0.3, arid_camp_weight * 0, mountain_camp_weight * 2, river_camp_weight * 2.5, forest_camp_weight * 0.3])
+var late_mid_weights = PackedFloat32Array([plains_camp_weight * 0, arid_camp_weight, mountain_camp_weight * 4, river_camp_weight * 0.5, forest_camp_weight * 0.3])
+var late_weights = PackedFloat32Array([plains_camp_weight * 0, arid_camp_weight * 3, mountain_camp_weight, river_camp_weight * 0.1, forest_camp_weight * 0])
 
 func generate_map() -> Array[Array]:
 	map_data = _generate_initial_grid()
-	var starting_points: Array[int]= [2,2,2,2]
+	var starting_points: Array[int]= [2,2,2,2,2,2]
 	
 	for j in starting_points:
 		var current_j := j
@@ -107,8 +111,25 @@ func _setup_camp_biomes() -> void:
 		if camp.next_camps.size() > 0:
 			camp.biome = Camp.Biome.PLAINS
 
-	for current_floor in map_data:
-		for camp: Camp in current_floor:
+	for i in range(FLOORS):
+		for camp: Camp in map_data[i]:
 			for next_camp: Camp in camp.next_camps:
 				if next_camp.biome == Camp.Biome.UNASSIGNED:
-					next_camp.biome = camp_biomes_array[rng.rand_weighted(weights)]
+					if i < 2:
+						next_camp.biome = camp_biomes_array[rng.rand_weighted(early_weights)]
+					elif  i >= 2 and i <=3:
+						next_camp.biome = camp_biomes_array[rng.rand_weighted(early_mid_weights)]
+					elif  i >= 4 and i <=6:
+						next_camp.biome = camp_biomes_array[rng.rand_weighted(mid_weights)]
+					elif  i >= 7 and i <=10:
+						next_camp.biome = camp_biomes_array[rng.rand_weighted(late_mid_weights)]
+					elif  i >= 11 and i <=15:
+						next_camp.biome = camp_biomes_array[rng.rand_weighted(late_weights)]
+					else:
+						next_camp.biome = camp_biomes_array[rng.rand_weighted(early_weights)]
+						
+	#for current_floor in map_data:
+		#for camp: Camp in current_floor:
+			#for next_camp: Camp in camp.next_camps:
+				#if next_camp.biome == Camp.Biome.UNASSIGNED:
+					#next_camp.biome = camp_biomes_array[rng.rand_weighted(weights)]
